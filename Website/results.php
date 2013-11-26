030a1223a8acd4a1677bc2f00aaa51bca83a5545
Brad Stevenson <BradStevenson@outlook.com>
2013-11-26 16:05:43 +0000

    Bug fixes.

--- Website/results.php
+++ Website/results.php
@@ -6,16 +6,16 @@
 <body>
        <?php
 
-            $server = mysql_connect("cs.nott.ac.uk","Ggp13_pkl", "CVNFTW");
+            $server = mysql_connect("mysql.cs.nott.ac.uk","gp13_pkl", "CVNFTW");
             $db =  mysql_select_db("gp13_pkl",$server);
-	    $searchTerm = $_GET["search"];
+	          $searchTerm = $_GET["search"];
             $query = mysql_query("select * from Papers WHERE year = ".$searchTerm);
         ?>
         <table class="striped">
             <tr class="header">
-                <td>Id</td>
-                <td>Name</td>
                 <td>Title</td>
+                <td>Author</td>
+                <td>Year</td>
             </tr>
             <?php
                while ($row = mysql_fetch_array($query)) {
