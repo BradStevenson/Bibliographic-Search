b13fd90ffc80a19f607d72824ba18c99a83a1438
Brad Stevenson <BradStevenson@outlook.com>
2013-12-09 09:34:38 +0000

    Rewrote using mysqli and mysql_connect is becomign depreciated.. and is kinda shitty.

--- Website/results.php
+++ Website/results.php
@@ -1,33 +1,63 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-	<title>Results</title>
+  <title>Results</title>
+  <link rel="stylesheet" type="text/css" href="results-style.css">
 </head>
 <body>
        <?php
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL | E_STRICT);
 
-            $server = mysql_connect("mysql.cs.nott.ac.uk","gp13_pkl", "CVNFTW");
-            $db =  mysql_select_db("gp13_pkl",$server);
-	          $searchTerm = $_GET["search"];
-            $query = mysql_query("select * from Papers WHERE year = ".$searchTerm);
+    $time = microtime();
+    $time = explode(' ', $time);
+    $time = $time[1] + $time[0];
+    $start = $time;
+    
+    $mysqli = new mysqli("mysql.cs.nott.ac.uk", "gp13_pkl", "CVNFTW", "gp13_pkl");
+    /* check connection */
+    if (mysqli_connect_errno()) {
+        echo "Connect failed: ".mysqli_connect_error();
+    }
+
+    $searchTerm = $_GET["search"];
+    if (preg_match("/^[0-9]{4}$/", $searchTerm, $matches)) {
+      $query = "SELECT * FROM Papers WHERE year = ".$searchTerm;
+    } else {
+      $query = "SELECT * FROM Papers WHERE title LIKE '%".$searchTerm."%'";
+    }
         ?>
-        <table class="striped">
-            <tr class="header">
-                <td>Title</td>
-                <td>Author</td>
-                <td>Year</td>
-            </tr>
+  <header>
+    <h4 class="text2">Project SciSearcher</h4>      
+    <form action="results.php" method="GET">
+      <input type="text" name="search" class="round" placeholder="Enter search criteria here."/>
+      <button type="submit" class="button">Search</button>
+    </form>
+  </header>
+        <table class="results">
             <?php
-               while ($row = mysql_fetch_array($query)) {
-                   echo "<tr>";
-                   echo "<td>".$row[title]."</td>";
-                   echo "<td>".$row[author]."</td>";
-                   echo "<td>".$row[year]."</td>";
-                   echo "</tr>";
-               }
-
+        $numRows = 0;
+                if ($result = $mysqli->query($query)) {
+            while ($row = $result->fetch_row()) {
+              echo "<tr>";
+              echo "<div id='result'><span id='title'>".$row[1].
+             "</span><span id='author'>".$row[2].
+             "</span><span id='year'>".$row[3].
+             "</span></div>";
+              echo "</tr>";
+              $numRows++;
+            }
+            /* free result set */
+            $result->close();
+        }
+        $mysqli->close();
+        $time = microtime();
+        $time = explode(' ', $time);
+        $time = $time[1] + $time[0];
+        $finish = $time;
+        $total_time = round(($finish - $start), 4);
+        echo "<div id='count'>".$numRows." matches in ".$total_time." seconds</div>";
             ?>
         </table>
 </body>
 </html>
-
