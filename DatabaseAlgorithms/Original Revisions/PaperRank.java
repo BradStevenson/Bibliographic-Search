f2b6be72a6e12712786cfaed805b65b2ca47c4a3
Brad Stevenson <BradStevenson@outlook.com>
2014-03-10 14:57:07 +0000

    removed some things

--- DatabaseAlgorithms/Original Revisions/PaperRank.java
+++ DatabaseAlgorithms/Original Revisions/PaperRank.java
@@ -28,7 +28,7 @@ public class PaperRank {
         String dbName = "SciSearcher";
         String driver = "com.mysql.jdbc.Driver";
         String userName = "root";
-        String password = "t4k34ch4nc3";
+        String password = "password";
         try {
             Class.forName(driver).newInstance();
             conn = DriverManager.getConnection(url+dbName,userName,password);
