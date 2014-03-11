6d51729cd04fd9ce90826d79189433807f5a1204
Brad Stevenson <BradStevenson@outlook.com>
2014-03-11 23:50:56 +0000

    FUlly working results page, with keyword search etc

--- Website/results.php
+++ Website/results.php
@@ -23,38 +23,93 @@
     }
 
     $searchTerm = $_GET["search"];
-    if (preg_match("/^[0-9]{4}$/", $searchTerm, $matches)) {
-      $query = "SELECT papers.title, papers.year, GROUP_CONCAT(authors.name), papers.abstract FROM papers, authors WHERE papers.id=authors.paperid AND papers.year = '".$searchTerm."' group by papers.title LIMIT 10;";
+    
+    if(isset($_GET["searchType"])){
+      $searchType = $_GET["searchType"];
+
+      switch ($searchType){
+        case "keyword":
+          $query = "SELECT urls.url, papers.title, papers.year, GROUP_CONCAT(authors.name), papers.abstract FROM papers, authors, urls, keywords WHERE papers.id=authors.paperid AND papers.id=urls.paperid AND papers.id=keywords.paperid AND keywords.keyword LIKE '%".$searchTerm."%' group by papers.title LIMIT 10;";
+          $querySize = "SELECT COUNT(*) FROM keywords WHERE keyword LIKE '%".$searchTerm."%'";
+          break;
+        case "title":
+          $query = "SELECT urls.url, papers.title, papers.year, GROUP_CONCAT(authors.name), papers.abstract FROM papers, authors, urls WHERE papers.id=authors.paperid AND papers.id=urls.paperid AND papers.title LIKE '%".$searchTerm."%' group by papers.title LIMIT 10;";
+          $querySize = "SELECT COUNT(*) FROM papers WHERE title LIKE '%".$searchTerm."%'";
+          break;
+        case "year" :
+          $query = "SELECT urls.url, papers.title, papers.year, GROUP_CONCAT(authors.name), papers.abstract FROM papers, authors, urls WHERE papers.id=authors.paperid AND papers.id=urls.paperid AND papers.year = '".$searchTerm."' group by papers.title LIMIT 10;";      
+          $querySize = "SELECT COUNT(*) FROM papers WHERE year = '".$searchTerm."'";
+          break;
+        case "author" :
+          $query = "SELECT urls.url, papers.title, papers.year, GROUP_CONCAT(authors.name), papers.abstract FROM papers, authors, urls WHERE papers.id=authors.paperid AND papers.id=urls.paperid AND authors.name LIKE '%".$searchTerm."%'  group by papers.title LIMIT 10;";
+          $querySize = "SELECT COUNT(*) FROM authors WHERE name LIKE '%".$searchTerm."%'";
+          break;
+        case "field" :
+          $query = "SELECT urls.url, papers.title, papers.year, GROUP_CONCAT(authors.name), papers.abstract FROM papers, authors, urls WHERE papers.id=authors.paperid AND papers.id=urls.paperid AND field LIKE '%".$searchTerm."%' LIMIT 10;";
+          $querySize = "SELECT COUNT(*) FROM papers WHERE field LIKE '%".$searchTerm."%'";
+          break;
+      }
     } else {
-      $query = "SELECT * FROM papers WHERE title LIKE '%".$searchTerm."%' LIMIT 10;";
+      if (preg_match("/^[0-9]{4}$/", $searchTerm, $matches)) {
+        $query = "SELECT urls.url, papers.title, papers.year, GROUP_CONCAT(authors.name), papers.abstract FROM papers, authors, urls WHERE papers.id=authors.paperid AND papers.id=urls.paperid AND papers.year = '".$searchTerm."' group by papers.title LIMIT 10;";
+        $querySize = "SELECT COUNT(*) FROM papers WHERE year = '%".$searchTerm."%'";
+        $searchType = 'year';
+      } else {
+        $query = "SELECT urls.url, papers.title, papers.year, GROUP_CONCAT(authors.name), papers.abstract FROM papers, authors, urls, keywords WHERE papers.id=authors.paperid AND papers.id=urls.paperid AND papers.id=keywords.paperid AND keywords.keyword LIKE '%".$searchTerm."%' group by papers.title LIMIT 10;";
+        $querySize = "SELECT COUNT(*) FROM keywords WHERE keyword LIKE '%".$searchTerm."%'";
+        $searchType = 'keyword';
+      }
     }
-    
-    $searchTerm = $_GET["search"];
-    // $searchOptions = $_GET["searchOptions"];
-    
-    
-    // switch ($searchOptions){
-    //   case "Title":
-    //     $query = "SELECT * FROM papers ORDER BY pr WHERE title = '".$searchTerm."' LIMIT 10;";
-    //     break;
-    //   case "Year" :
-    //     $query = "SELECT * FROM papers ORDER BY pr WHERE year = '".$searchTerm."'"; 
-    //     break;
-    //   case "Author" :
-    //     $query =  "SELECT * FROM papers ORDER BY pr WHERE paper.id = SELECT authors.paperid FROM authors WHERE authors.name = '".$searchTerm."' LIMIT 10;";
-    //     break;
-    //   case "Field" :
-    //     $query = "SELECT * FROM papers ORDER BY pr WHERE papers.field = '".$searchTerm."' LIMIT 10;";
-    // }
     ?>
 
   <header>
-    <h1 class="pageTitle">Project SciSearcher</h4>      
+    <h2 class="pageTitle1">Project</h2>
+    <h1 class="pageTitle2">SciSearcher</h4>      
     <form action="results.php" method="GET">
-      <input type="text" name="search" class="round" value="<?php echo $searchTerm ?>" required/>
+      <input type="text" name="search" class="round" value="<?php echo $searchTerm ?>"/>
       <button type="submit" class="button">Search</button>
-    </form>
-  </header>
+    <div class="searchOptions">
+      <input type="radio" name="searchType" id="Keyword" value='keyword' onchange='this.form.submit()'
+      <?php 
+        if($searchType == 'keyword') {
+          echo "checked";
+        }
+      ?>
+      />
+        <label for="Keyword">Keyword</label>
+      <input type="radio" name="searchType" id="Title" value='title' onchange='this.form.submit()'
+      <?php 
+        if($searchType == 'title') {
+          echo "checked";
+        }
+      ?>
+      />
+        <label for="Title">Title</label>
+      <input type="radio" name="searchType" id="Year" value='year' onchange='this.form.submit()'
+      <?php 
+        if($searchType == 'year') {
+          echo "checked";
+        }
+      ?>
+      />
+        <label for="Year">Year</label>
+      <input type="radio" name="searchType" id="Author" value='author' onchange='this.form.submit()'
+      <?php 
+        if($searchType == 'author') {
+          echo "checked";
+        }
+      ?>
+      />
+        <label for="Author">Author</label>
+      <input type="radio" name="searchType" id="Field" value='field' onchange='this.form.submit()'
+      <?php 
+        if($searchType == 'field') {
+          echo "checked";
+        }
+      ?>
+      />
+        <label for="Field">Field</label>
+
 
   <?php
     $resultsHTML = '';
@@ -64,15 +119,15 @@
         while ($row = $result->fetch_row()) {
        	  	$resultsHTML .= "<tr>";
 		      	$resultsHTML .= "<td  class='result'>";
-          	$resultsHTML .= "<h1 class='title'>".$row[0]."</h1>";
+          	$resultsHTML .= "<a class='title' href='".$row[0]."'>".$row[1]."</a>";
             $resultsHTML .= "<div class='resultInfo'>";
-            $resultsHTML .= "<p class='author'>".$row[2]."</p>";
-            $resultsHTML .= "<p class='year'>".$row[1]."</p>";
+            $resultsHTML .= "<p class='author'>".$row[3]."</p>";
+            $resultsHTML .= "<p class='year'>".$row[2]."</p>";
             $resultsHTML .= "</div>";
-            if ($row[3] == ' ') {
+            if ($row[4] == ' ') {
               $resultsHTML .= "<p class='abstract'><span class='placeholder'>No abstract available.</span></p>";
             }else {
-              $resultsHTML .= "<p class='abstract'>".$row[3]."</p>";
+              $resultsHTML .= "<p class='abstract'>".$row[4]."</p>";
             }
 
           	$resultsHTML .= "</td>";
@@ -82,20 +137,24 @@
             
         $result->close();
   	}
-    $mysqli->close();
+    $resultsHTML .= "</table>";
+
     $time = microtime();
     $time = explode(' ', $time);
     $time = $time[1] + $time[0];
     $finish = $time;
     $total_time = round(($finish - $start), 4);
-	  $resultsHTML .= "</table>";
-    echo "<div id='count'>".$numRows." matches in ".$total_time." seconds</div>";
+
+    $sizeResult = $mysqli->query($querySize);
+    $size = $sizeResult->fetch_row();
+    echo "<div id='count'>".$numRows." matches of ".$size[0]." in ".$total_time." seconds</div>";
+    $sizeResult->close();
+    $mysqli->close();
+    echo "</div>";
+    echo "</form>";
+    echo "</header>";
     echo $resultsHTML;   
 	?>
         
 </body>
-<script type="text/javascript" src="js/resultsScript.js"></script>
-<script type="text/javascript" src="js/queries.js"></script>
-<script type="text/javascript" src="jquery-1.8.3.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.js"></script>
 </html>
