a16fa3f3f5f83dac1f10c5b7e747dba09ab457cf
Brad Stevenson <BradStevenson@outlook.com>
2014-03-19 16:22:50 +0000

    Moved to prepared statments to save resources. Ready to run now.

--- DatabaseAlgorithms/PaperRank.java
+++ DatabaseAlgorithms/PaperRank.java
@@ -16,6 +16,9 @@ public class PaperRank {
     public static void main(String[] args) {
 	startConnection();
 	maxPapers = getNumberOfPapers();
+
+	System.out.println(maxPapers + " papers found");
+
 	pageRanks = new double[maxPapers];
 	outboundCitations = new int[maxPapers];
 	// Create Hashmap of index to paperid string
@@ -24,18 +27,31 @@ public class PaperRank {
 	// initialise the whole array to 1
 	Arrays.fill(pageRanks, 1.0);
 	// Fill outboundCitations
-	for (int i = 0; i < outboundCitations.length; i++) {
-	    outboundCitations[i] = getOutboundCitation(i);
+	try {
+	    for (int i = 0; i < outboundCitations.length; i++) {
+		outboundCitations[i] = getOutboundCitation(i);
+	    }
+	} catch (SQLException e) {
+	    System.err.println("Getting outbound citation failed.");
+	    e.printStackTrace();
 	}
 
 	// Need to call ~20 times.
-	pageRank();
+	for (int i = 1; i <= 20; i++) {
+	    pageRank();
+	}
 
+	try {
+	    insertPageRanks();
+	} catch (SQLException e) {
+	    System.err.println("Inserting PageRanks failed.");
+	    e.printStackTrace();
+	}
 	endConnection();
     }
 
     private static void startConnection() {
-	String url = "jdbc:mysql://sproj08.cs.nott.ac.uk:3306";
+	String url = "jdbc:mysql://sproj08.cs.nott.ac.uk:3306/";
 	String dbName = "SciSearcher";
 	String driver = "com.mysql.jdbc.Driver";
 	String userName = "root";
@@ -45,6 +61,7 @@ public class PaperRank {
 	    conn = DriverManager
 		    .getConnection(url + dbName, userName, password);
 	    st = conn.createStatement();
+	    System.out.println("Connected");
 	} catch (Exception e) {
 	    e.printStackTrace();
 	}
@@ -65,65 +82,90 @@ public class PaperRank {
 	// for every pagerank
 	for (int i = 0; i < pageRanks.length; i++) {
 	    culmativeRank = 0;
-	    for (int paper : papersCiting(i)) {
-		culmativeRank += pageRanks[paper] / outboundCitations[paper];
+	    try {
+		for (int paper : papersCiting(i)) {
+		    culmativeRank += pageRanks[paper]
+			    / outboundCitations[paper];
+		}
+		pageRanks[i] = (1 - d) + d * (culmativeRank);
+	    } catch (SQLException e) {
+		System.err
+			.println("Error getting papers Citing for paper " + i);
+		e.printStackTrace();
 	    }
-	    pageRanks[i] = (1 - d) + d * (culmativeRank);
 	}
     }
 
-    private static int getOutboundCitation(int id) {
+    private static int getOutboundCitation(int id) throws SQLException {
+	PreparedStatement selectNC = null;
 	int citationCount = 0;
 	String paperID = paperIDs.get(id);
 
 	// MYSQL QUERY FIND HOW MANY CITATIONS
-	String sql = "Select numcites from papers where paperID ='" + paperID
-		+ "';";
+	String selectSQL = "SELECT numCites FROM papers WHERE id =?;";
 	try {
-	    ResultSet results = statement.executeQuery(sql);// execute statement
-	    results.next();
+	    selectNC = conn.prepareStatement(selectSQL);
+	    selectNC.setString(1, paperID);
+	    ResultSet rs = selectNC.executeQuery();// execute statement
+	    rs.next();
 
-	    return results.getInt(1);
+	    citationCount = rs.getInt("numCites");
 
 	} catch (SQLException e) {
 	    e.printStackTrace();
+	} finally {
+	    if (selectNC != null) {
+		selectNC.close();
+	    }
+
+	    System.out.println("Outbound citations gathered for "
+		    + paperIDs.get(id));
+
+	    return citationCount;
 	}
-	return citationCount;
     }
 
-    public static int getNumberOfPapers() { // not including marked deleted
-					    // mails
+    public static int getNumberOfPapers() {
+	int num = 0;
 	String sql = "Select COUNT(*) AS count FROM Papers";
 	try {
 	    ResultSet results = statement.executeQuery(sql);// execute statement
-	    while (results.next()) {
-		return results.getInt("count");
-	    }
+	    results.next();
+	    num = results.getInt("count");
 	} catch (SQLException e) {
 	    e.printStackTrace();
 	}
 
-	return 0;
+	return num;
     }
 
-    private static ArrayList<Integer> papersCiting(int id) {
+    private static ArrayList<Integer> papersCiting(int id) throws SQLException {
 	// find every paperID that cites our id parameter
+	PreparedStatement selectID = null;
 	ArrayList<Integer> citesArray = new ArrayList<>();
 	String paperID = paperIDs.get(id);
-	String sql = "SELECT paperid FROM citations WHERE title LIKE (SELECT title FROM papers WHERE id = "
-		+ paperID + ");";
+
+	String selectSQL = "SELECT paperid FROM citations WHERE title LIKE (SELECT title FROM papers WHERE id = ?);";
 	try {
-	    ResultSet results = statement.executeQuery(sql);
-	    while (results.next()) {
-		String pID = results.getString("paperid");
+	    selectID = conn.prepareStatement(selectSQL);
+	    selectID.setString(1, paperID);
+	    ResultSet rs = selectID.executeQuery();
+	    while (rs.next()) {
+		String pID = rs.getString("paperid");
 		citesArray.add(getIndexForID(pID));
 	    }
 
 	} catch (SQLException e) {
 	    e.printStackTrace();
-	}
+	} finally {
+	    if (selectID != null) {
+		selectID.close();
+	    }
 
-	return citesArray;
+	    System.out.println("Found all papers citing " + paperIDs.get(id));
+
+	    return citesArray;
+	}
     }
 
     private static HashMap<Integer, String> getPaperIDs(int size) {
@@ -143,10 +185,13 @@ public class PaperRank {
 	    e.printStackTrace();
 	}
 
+	System.out.println("All " + size + " paper IDs collected");
+
 	return map;
     }
 
     private static void insertPageRanks() throws SQLException {
+	System.out.println("Beginning DB update of PageRanks");
 	PreparedStatement updatePR = null;
 	String updateString = "UPDATE papers SET pagerank=? WHERE id = ?;";
 	try {
@@ -173,6 +218,7 @@ public class PaperRank {
 		updatePR.close();
 	    }
 	    conn.setAutoCommit(true);
+	    System.out.println("PageRanks updated in DB");
 	}
     }
 
